0b4f6024b81af07522f8fc99807f0658244434c3
davydocsurg <davydocsurg@gmail.com>
2024-01-29 01:16:22 +0100

    chore: setup API gateway

---
+++ gateway/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import proxy from "express-http-proxy";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const auth = proxy("http://localhost:8081");
+const messages = proxy("http://localhost:8082");
+
+app.use("/auth", auth);
+app.use("/messages", messages);
+
+const server = app.listen(8080, () => {
+    console.log("Gateway is Listening to Port 8080");
+});
+
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            console.info("Server closed");
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error: unknown) => {
+    console.error(error);
+    exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
